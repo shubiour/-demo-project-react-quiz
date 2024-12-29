3c0e88155f24704d0d03720f0b096ecde248fa8a
shubiour <sv95.shuvo@gmail.com>
2024-12-29 21:08:58 +0600

    feat-quiz shuffling enabled

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
 import QUESTIONS from "../../questions.js";
+import quizCompleteImage from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
-
   const activeQuestionIndex = userAnswers.length;
+  const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnsser(answer) {
     return setUserAnswers((prevUserAnsers) => [...prevUserAnsers, answer]);
   }
+
+  if(quizIsOver){
+    return <div id="summary">
+      <img src={quizCompleteImage} alt="Quiz complete" />
+      <h2>Quiz is completed</h2>
+    </div>
+  }
+
+  const shuffledAnswers =  [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswers.map((answer, index) => (
             <li key={index} className="answer">
               <button onClick={() => handleSelectAnsser(answer)}>
                 {answer}
